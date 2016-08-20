7037d87488261140f8998dea4f7ba4be1d2991dd
Alexander Litty <github@alexlitty.com>
2016-08-20 00:29:13 -0700

    Simple compilation architecture.

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        paths: {
+            src: {
+                js: 'client/**/*.js'
+            },
+
+            dest: {
+                js: 'www/beekeeper.min.js'
+            }
+        },
+
+        htmlmin: {
+            www: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+
+                files: {
+                    'www/index.html': 'client/index.html'
+                }
+            }
+        },
+
+        concat: {
+            js: {
+                options: {
+                    separator: ';'
+                },
+                src: '<%= paths.src.js %>',
+                dest: '<%= paths.dest.js %>'
+            }
+        },
+
+        uglify: {
+            options: {
+                compress: true,
+                mangle: true,
+                sourceMap: false
+            },
+
+            www: {
+                src: '<%= paths.src.js %>',
+                dest: '<%= paths.dest.js %>'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+    grunt.registerTask('default', ['htmlmin', 'concat', 'uglify']);
+}
