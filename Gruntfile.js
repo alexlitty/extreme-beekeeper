176e7a8ddb8ac32a90c67ad758def3535d3e3b89
Alexander Litty <github@alexlitty.com>
2016-08-28 15:21:33 -0700

    Added mkdir to default grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,5 +69,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['htmlmin', 'concat', 'uglify']);
+    grunt.registerTask('default', ['mkdir', 'htmlmin', 'concat', 'uglify']);
 }
